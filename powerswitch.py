cca2e292940fb13fc9a01fe7e8c0bf4d1e87b454
nahidalam <shapla@gmail.com>
2014-10-07 10:54:05 -0700

    Create powerswitch.py

---
+++ powerswitch.py
@@ -0,0 +1,24 @@
+import time
+import RPi.GPIO as io
+io.setmode(io.BCM)
+
+power_pin = 23
+
+io.setup(power_pin, io.OUT)
+io.output(power_pin, False)
+
+while True:
+    nb = raw_input('Choose a number: ')
+    try:
+    number = int(nb)
+except ValueError:
+    print("Invalid number")
+    if number>0:
+        print("POWER ON")
+        io.output(power_pin, True)
+        time.sleep(20);
+    else:
+        print("POWER OFF")
+        io.output(power_pin, False)
+        time.sleep(5)
+    time.sleep(1)
